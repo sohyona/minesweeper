ffb35ab4e663395cb2e4a97b27c09f2c07fa0f23
Seo Hyona <hyunhaseo@gmail.com>
2020-01-12 00:50:11 +0900

    before implement timer

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -4,20 +4,17 @@ const initialState = newBoard ();
 const boardReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'OPEN_CELL': {
-      console.log ('OPEN_CELL');
       let newState = state;
       newState[action.y][action.x].isOpen = true;
       return newState;
     }
     case 'TOGGLE_FLAG': {
-      console.log ('TOGGLE_FLAG');
       let newState = state;
       newState[action.y][action.x].isFlag = !newState[action.y][action.x]
         .isFlag;
       return newState;
     }
     case 'RESET_BOARD': {
-      console.log ('RESET_BOARD');
       return newBoard ();
     }
     default:
